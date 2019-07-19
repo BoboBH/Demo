d51078264eacff5cf6974a96528c71bf0c0d58b8
Bobo Huang <bobo.huang@reorientgroup.com>
2019-07-19 10:23:48 +0800

    deploy aspnetcore on docker;

---
+++ CSharp/AppDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

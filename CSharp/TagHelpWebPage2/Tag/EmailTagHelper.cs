e391d57f1cbd1893aa0327df0b79114242820a80
Bobo Huang <bobo.huang@reorientgroup.com>
2019-08-12 17:10:59 +0800

    add tag help project;

---
+++ CSharp/TagHelpWebPage2/Tag/EmailTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelpWebPage2.Tag
+{
+    [HtmlTargetElement("email", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement(Attributes = "email", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class EmailTagHelper:TagHelper
+    {
+        public string MailTo { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    // Replaces <email> with <a> tag
+            var address = MailTo + "@" + "@yff.com";
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            string mailTo = content.GetContent() + "@yff.com";
+            output.Attributes.SetAttribute("href", "mailto:" + mailTo);
+            output.Content.SetContent(mailTo);
+        }
+    }
+}
---
+++ CSharp/TagerHelperWebPage/HtmlTag/EmailTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagerHelperWebPage.HtmlTag
+{
+    [HtmlTargetElement("email")]
+    [HtmlTargetElement(Attributes="email")]
+    public class EmailTagHelper: TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            string content = output.Content.GetContent();
+            string mailTo = content +  "@yff.com";
+            output.Attributes.SetAttribute("href", "mailto:" + mailTo);
+            output.Content.SetContent(mailTo);
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            string mailTo = content + "@yff.com";
+            output.Attributes.SetAttribute("href", "mailto:" + mailTo);
+            output.Content.SetContent(mailTo);
+        }
+    }
+}
